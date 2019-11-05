6eaf0781b13d7dc6a09f5b45807a91c3203de3cc
Alvarsson <a-alvarsson@hotmail.com>
2019-11-05 16:46:50 +0100

    index page fixed

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+kategorier = ["Barrträd", "Lövträd", "Små träd","Stora träd","Gamer-träd","Wannabee-träd","Träd från kända serier"]
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", ArrayMedTräd = kategorier)
+
 
 @app.route("/login")
 def login():
     return render_template("login.html")
+
+
 
 artiklar = ["edward", "albin", "axel", "blabla", "hejhej"]
 @app.route("/kategori")
