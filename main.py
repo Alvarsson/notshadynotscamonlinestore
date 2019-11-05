287748534ea1263cb7907d0a5fd8dd36ca211141
Edward <edward.kallstedt@gmail.com>
2019-11-05 12:14:37 +0100

    add category page

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def home():
 def login():
     return render_template("login.html")
 
+artiklar = ["edward", "albin", "axel", "blabla", "hejhej"]
+@app.route("/kategori")
+def kategori():
+    return render_template("kategori.html", artiklar = artiklar)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
