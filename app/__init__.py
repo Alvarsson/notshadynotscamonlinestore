4b829b2657aef8ac4b06d8128e4d91f599343aff
Edward <edward.kallstedt@gmail.com>
2019-11-11 11:50:24 +0100

    added db config options

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask, Blueprint
 from flask_mysqldb import MySQL
+from config import Config
 
 db = MySQL()
 
-def create_app():
+def create_app(config=Config):
     app = Flask(__name__)
+    app.config.from_object(config)
+
+
     db.init_app(app)
 
     from app.main import main_bp
---
+++ config.py
@@ -0,0 +1,5 @@
+class Config(object):
+    MYSQL_USER = '';
+    MYSQL_PASSWORD = '';
+    MYSQL_DB = '';
+
