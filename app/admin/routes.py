b71a5935cb9ac45a69437acea4977c9770fda0d9
Alvarsson <a-alvarsson@hotmail.com>
2019-11-21 17:46:37 +0100

    Påbörjat funktion för att kunna ta bort och ändra på existerande artiklar

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -11,10 +11,18 @@ def admin():
 @bp.route("/admin/articles", methods=['POST', 'GET'])
 def adminArticles():
     cur = db.connection.cursor()
-    cur.execute('''SELECT category_name, category_id FROM categories''')
+    cur.execute("SELECT category_name, category_id FROM categories")
     categoryArray = []
-    for i in cur.fetchall():
-        categoryArray.append(i)
+    for cat in cur.fetchall():
+        categoryArray.append(cat)
+
+    cur.execute("SELECT article_number, article_name, stock_quantity, price FROM articles")
+    articlesArray = []
+    for art in cur.fetchall():
+        articlesArray.append(art)
+    
+    #print(cur.fetchall())
+    
     
     if request.method == 'POST':
         #Submit för att lägga till artiklar
@@ -24,14 +32,17 @@ def adminArticles():
             stockAmount = request.form['stock']
             price = request.form['price']
             url = "('" + request.form['url'] + "')"
-            desc = "('" + request.form['description'] + "')"
+            desc = request.form['description']
 
-            cur.execute("INSERT INTO articles (article_name, category, price, stock_quantity, picture_url, article_description) VALUES ("+ articleName +",(SELECT category_id FROM categories WHERE category_name="+ chooseCat +"),"+ price + ","+ stockAmount +","+ url +","+ desc +");" )
+            cur.execute("INSERT INTO articles (article_name, category, price, stock_quantity, picture_url) VALUES ("+ articleName +",(SELECT category_id FROM categories WHERE category_name="+ chooseCat +"),"+ price + ","+ stockAmount +","+ url +");" )
+            if desc != "":
+                desc = "('" + request.form['description'] + "')"
+                cur.execute("INSERT INTO article_description (description, description_number ) VALUES ("+ desc +", (SELECT article_number FROM articles WHERE article_name="+ articleName +"));")
             db.connection.commit()
             cur.close()
             return redirect(url_for('admin.adminArticles'))
 
-    return render_template("admin/articles.html", categories = categoryArray)
+    return render_template("admin/articles.html", categories = categoryArray, articles = articlesArray)
 
 @bp.route("/admin/categories", methods=['POST', 'GET'])
 def adminCategories():
