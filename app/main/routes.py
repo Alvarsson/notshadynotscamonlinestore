06ec2ab00d6c3b7838b8d02f0131810907bcb3a4
Alvarsson <a-alvarsson@hotmail.com>
2019-11-25 22:07:45 +0100

    Navbar med lite iconer samt början på user funktioner

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,7 +5,8 @@ from app import db
 
  #Static test input
 artiklar = [["Tall",3,239],["Ek",13,2329],["Lönn",31,2139]]
-artiklar = ["edward", "albin", "axel", "blabla", "hejhej"]
+artiklar = [["edward",'Username:'],["albin", 'First name:'], ["axel", 'Sur name:'], ["blabla", 'Email:'], ["hejhej", 'Adress:']]
+testy = ['Username', 'First name', 'Sur name', 'Email', 'Adress', 'Password']
 #kategorier = ["Barrträd", "Lövträd", "Små träd","Stora träd","Gamer-träd","Wannabee-träd","Träd från kända serier"]
 
 @bp.route("/test")
@@ -17,13 +18,14 @@ def testsite():
 @bp.route("/")
 def home():
     cur = db.connection.cursor()
-    cur.execute('''SELECT * FROM categories ORDER BY category_name''')
+    cur.execute("SELECT * FROM categories ORDER BY category_name")
 
     categories = []
     for i in cur.fetchall():
         category = (int(i[0]),i[1])
         
         categories.append(category)
+    print(categories)
     return render_template("index.html", ArrayMedTräd = categories)
 
 
@@ -34,7 +36,7 @@ def category(category_id):
     cur.execute("SELECT * FROM articles INNER JOIN categories ON articles.category=categories.category_id WHERE categories.category_id=" + str(category_id)) # Can't wait for that sweet, sweet SQL Injection right here.
     result = list()
     for i in cur.fetchall():
-        result.append(i)
+        result.append(i(index))
 
     #print(result)
     
@@ -51,6 +53,19 @@ def article(article_number):
 
 @bp.route("/user")
 def user():
+    #Få in inlogginformation om användare här!
+    cur = db.connection.cursor()
+    cur.execute("SELECT user_name, first_name, last_name, mail, adress FROM users WHERE customer_id="+ str(2))
+    userResult = []
+    for i in cur.fetchall():
+        userResult.append(i)
+    
+
+    return render_template("user.html", testy = testy, userResult = userResult)
+
+@bp.route("/user/cart")
+def cart():
+    
     return render_template("user.html", artiklar = artiklar)
 
 
