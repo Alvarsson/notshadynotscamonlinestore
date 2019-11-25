c2cba6a302c44922c308e1e63dac0fe53ed58559
altzin <albtho-5@student.ltu.se>
2019-11-25 22:23:28 +0100

    bug in admin articles

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -129,3 +129,64 @@ def adminUsers():
             cur.close()
             return redirect(url_for('admin.adminUsers'))
     return render_template("admin/users.html", users=userArray)
+
+
+# @bp.route("/admin/articles", methods=['POST', 'GET'])
+# def adminArticles():
+#     cur = db.connection.cursor()
+#     cur.execute("SELECT category_name, category_id FROM categories")
+#     categoryArray = []
+#     for cat in cur.fetchall():
+#         categoryArray.append(cat)
+
+#     cur.execute(
+#         "SELECT article_number, article_name, stock_quantity, price FROM articles")
+#     articlesArray = []
+#     for art in cur.fetchall():
+#         articlesArray.append(art)
+
+#     if request.method == 'POST':
+#         # Submit för att lägga till artiklar
+#         if request.form['submit'] == 'addarticle':
+#             chooseCat = "('" + request.form['chooseCategory'] + "')"
+#             articleName = "('" + request.form['article'] + "')"
+#             stockAmount = request.form['stock']
+#             price = request.form['price']
+#             url = "('" + request.form['url'] + "')"
+#             desc = request.form['description']
+
+#             cur.execute("INSERT INTO articles (article_name, category, price, stock_quantity, picture_url) VALUES (" + articleName +
+#                         ",(SELECT category_id FROM categories WHERE category_name=" + chooseCat + ")," + price + "," + stockAmount + "," + url + ");")
+#             if desc != "":
+#                 desc = "('" + request.form['description'] + "')"
+#                 cur.execute("INSERT INTO article_description (description, description_number ) VALUES (" +
+#                             desc + ", (SELECT article_number FROM articles WHERE article_name=" + articleName + "));")
+#             db.connection.commit()
+#             cur.close()
+#             return redirect(url_for('admin.adminArticles'))
+#         # Ta bort artikel
+#         elif request.form['submit'] == 'remove':
+#             removeArticle = request.form['removefield']
+#             cur.execute(
+#                 "DELETE FROM articles WHERE article_number=" + removeArticle + ";")
+#             db.connection.commit()
+#             cur.close()
+#             return redirect(url_for('admin.adminArticles'))
+#         # Ändra artikelinformation
+#         elif request.form['submit'] == 'edit':
+#             editID = request.form['editfield']
+#             newName = "('" + request.form['newname'] + "')"
+#             newStock = request.form['newstock']
+#             newPrice = request.form['newprice']
+#             newDescription = request.form['newdescription']
+#             cur.execute("UPDATE articles SET article_name=" + newName + ", stock_quantity=" +
+#                         newStock + ", price=" + newPrice + " WHERE article_number= " + editID + ";")
+#             if newDescription != "":
+#                 newDescription = "('" + request.form['newdescription'] + "')"
+#                 cur.execute("UPDATE article_description SET description=" +
+#                             newDescription + " WHERE description_number= " + editID + ";")
+#             db.connection.commit()
+#             cur.close()
+#             return redirect(url_for('admin.adminArticles'))
+
+#     return render_template("admin/articles.html", categories=categoryArray, articles=articlesArray)
