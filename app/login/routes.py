30bed372e34af6ed51ab25a78e208f8f106ec8cc
Alvarsson <a-alvarsson@hotmail.com>
2019-11-26 10:03:41 +0100

    div

--- app/login/routes.py
+++ app/login/routes.py
@@ -10,7 +10,7 @@ def login():
         return redirect('/')
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        user = User.get_user(username=login_form.username.data)
+        user = Useru.get_user(username=login_form.username.data)
         if not user or not user.check_password(login_form.password.data):
             flash("User does not exist or password is wrong.")
             return redirect(url_for("login.login"))
