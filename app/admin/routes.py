32d871843a9db7c8e69748a7929e4dbfd1ea13b0
Albin Thomsson <albtho-5@student.ltu.se>
2019-11-26 10:22:48 +0100

    fixat articles i admin, nu funkar description

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -45,15 +45,13 @@ def adminArticles():
         articleName = str(addArticle.name.data) 
         stockAmount = str(addArticle.stock.data)
         price = str(addArticle.price.data)
-        url = "('" + str(addArticle.url.data) + "')"
-        # desc = str(addArticle.description.data)
+        url = str(addArticle.url.data) 
+        desc = str(addArticle.description.data)
 
         cur.execute("INSERT INTO articles (article_name, category, price, stock_quantity, picture_url) VALUES ('" + articleName + "','" + chooseCat +
-                     "', '" + price+ "' ,  '" + stockAmount + "'   ,'"+ url + "');")
-        # if desc != "": 
-        #     desc = "('" + request.form['description'] + "')"
-        #     cur.execute("INSERT INTO article_description (description, description_number ) VALUES (" +
-        #                 desc + ", (SELECT article_number FROM articles WHERE article_name=" + articleName + "));")
+                     "', '" + price+ "' ,  '" + stockAmount + "'   ,'" + url + "');")
+        if desc != "": 
+            cur.execute("INSERT INTO article_description (art_description, description_id ) VALUES ('" + desc + "', (SELECT article_number FROM articles WHERE article_name='" + articleName + "'));")
         db.connection.commit()
         cur.close()
         return redirect(url_for('admin.adminArticles'))
