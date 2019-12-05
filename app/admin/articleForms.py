2fefd9dd023a627e068bff861b0ba31c59f452e8
Albin Thomsson <albtho-5@student.ltu.se>
2019-12-05 22:08:42 +0100

    Nu är allt i admin iaf helt som det skall srry för dröjsmål mina bekanta

--- app/admin/articleForms.py
+++ app/admin/articleForms.py
@@ -2,17 +2,74 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
+from app import db
 
-class AddArticleForm(FlaskForm):
-
-    #hoicess = [(34, '1'), (31, '22'), (1, 'Barrträd'), (24, 'Eduardo'), (5, 'Gamer-träd'), (2, 'Lövträd'), (33, 'NUDÅ!?'), (3, 'Små träd'), (4, 'Stora träd'), (7, 'Träd från kända serier'), (6, 'Wannabee-träd'), (39, 'aa'), (44, 'aaa232'),
-                #(32, 'aaaa'), (38, 'aaaaaaaaaaaaaaaaa'), (27, 'aaakkkiiariir'), (28, 'ddd'), (18, 'hejsan'), (30, 'kkakaaka'), (46, 'naaaa'), (48, 'nu då!?'), (41, 'okidoke'), (42, 'okieo'), (49, 'rrrr'), (47, 's'), (19, 'tjosan'), (29, 'tysk')]
-
+class ArticleForm(FlaskForm):
     name = StringField('Article name', validators=[DataRequired()])
     category = SelectField('Category ID', validators=[DataRequired()], coerce=int)
     stock = IntegerField('Stock', validators=[DataRequired()])
     price = IntegerField('Price', validators=[DataRequired()])
     url = StringField('URL')
     description = StringField('Description')
-    submitAddArticle = SubmitField("Add Article")
+    submitArticle = SubmitField("Add Article")
+
+ 
+    def __init__(self,article_number=None):
+        super(ArticleForm, self).__init__() 
+        cur = db.connection.cursor()
+        cur.execute("SELECT category_id,category_name FROM categories")
+        categoryArray = []
+        for cat in cur.fetchall():
+            categoryArray.append(cat)
+
+        self.category.choices = categoryArray
+
+        if article_number != None and self.name.data == None: #för att stoppa att skriva över form input från user... 
+            self.addPlaceholders(article_number)
+
+
+    ########################################## EDWARD SAY ME, WHY NOT 2 CONSTRUCTORS I LOSE MIND #########################################
+    #behöver nog inte ens vara static. eller? 
+    #@staticmethod
+    def addPlaceholders(self,article_number): #vi vill bara göra detta med ett artikel nummer så none behövs ej
+        #print("om du kommit in i edit specific article delen, så påbörjar en annorlunda articleform")
+        cur = db.connection.cursor()
+
+        cur.execute("""SELECT article_number,stock_quantity,category_id,article_name,price,picture_url,art_description FROM articles
+         left JOIN categories ON articles.category=categories.category_id 
+         left join article_description on articles.article_number=article_description.description_id 
+         where article_number =""" + str(article_number) + ";")
+        
+        query = cur.fetchone()
+        db.connection.commit()
+        cur.close()
+        print(query)
+
+        #editArticle=ArticleForm()
+
+        self.name.data = query[3]
+        self.category.data = query[2]
+        self.stock.data = query[1]
+        self.price.data = query[4]
+        self.url.data = query[5] if query[5] != None else ""
+        self.description.data = query[6] if query[6] != None else ""
+
+        #return editArticle
+
+
+class RemoveArticleForm(FlaskForm):
+    article = IntegerField('Article ID')
+    submitRemoveArticle = SubmitField("Remove")
+
+
+# class EditArticleForm(FlaskForm):
+#     name = StringField('Article name', validators=[DataRequired()])
+#     category = SelectField('Category ID', validators=[DataRequired()], coerce=int)
+#     stock = IntegerField('Stock', validators=[DataRequired()])
+#     price = IntegerField('Price', validators=[DataRequired()])
+#     url = StringField('URL')
+#     description = StringField('Description')
+#     submitAddArticle = SubmitField("Add Article")
+
+
 
--- app/admin/routes.py
+++ app/admin/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, request
 from app.admin import admin_bp as bp
 from app import db
 from app.admin.forms import AddCategoryForm, RemoveCategoryForm, EditCategoryForm
-from app.admin.articleForms import AddArticleForm
+from app.admin.articleForms import ArticleForm,RemoveArticleForm
 
 
 artiklar = ["edward", "albin", "axel", "blabla", "hejhej"]
@@ -13,43 +13,94 @@ def admin():
     return render_template("admin/overview.html", artiklar=artiklar)
 
 
+@bp.route("/admin/editArticle/<int:article_number>", methods=['POST', 'GET'])
+def adminEditArticle(article_number):
+
+    editArticle=ArticleForm(article_number)
+    
+    if editArticle.validate_on_submit() and editArticle.submitArticle.data:
+        
+        cur = db.connection.cursor()
+        print(editArticle.name.data)
+
+        #editID = str(editArticle.article_number) tycker inte det är en bra ide att låta folk göra detta. exempel hoppa till 100 när count är 23 osv
+        newName = str(editArticle.name.data)
+        newStock = str(editArticle.stock.data)
+        newPrice = str(editArticle.price.data)
+        
+        newPicture = str(editArticle.url.data)
+        ###########################glöm inte kategori. problem med att nuvarande inte visas när man skriver .data delen...
+        cur.execute("UPDATE articles SET article_name= " + "'" + newName + "'" +", stock_quantity=" +  newStock + ", price=" + newPrice + ",picture_url='" + newPicture  +  "' WHERE article_number= " + str(article_number) +";")
+        #cur.execute("UPDATE articles SET article_name= " + "'" + newName + "' WHERE article_number= " + str(article_number) +";")
+
+        #cur.execute("""UPDATE articles SET article_name = "TallTall", stock_quantity= 123, price= 124, picture_url= "texas.com"  WHERE article_number=2;""")
+        
+        newDescription = str(editArticle.description.data)
+        if newDescription != "":
+            #upsert, så infoga ny rad om ingen finns, annars uppdatera googla det
+            cur.execute("INSERT INTO article_description (description_id,art_description) VALUES ( " + str(article_number) + ",'" + newDescription+ "') ON DUPLICATE KEY UPDATE art_description ='" + newDescription + "';")
+            print("Uppdaterar eller infogar nya data i description tabellen")
+
+        db.connection.commit()
+        cur.close()
+        return redirect(url_for('admin.adminArticles'))
+        
+    return render_template("admin/editArticle.html",editArticleForm = editArticle, articleNumber = article_number)
+
+
+
 @bp.route("/admin/articles", methods=['POST', 'GET'])
 def adminArticles():
+
     cur = db.connection.cursor()
-    cur.execute("SELECT  category_id,category_name FROM categories")
-    categoryArray = []
-    for cat in cur.fetchall():
-        categoryArray.append(cat)
 
-    cur.execute(
-        "SELECT article_number, article_name, stock_quantity, price FROM articles")
+    #Article list in adminview
+    cur.execute("SELECT article_number,stock_quantity,category_name,article_name,price FROM articles INNER JOIN categories ON articles.category=categories.category_id;")
     articlesArray = []
-    for art in cur.fetchall():
-        articlesArray.append(art)
+    for article in cur.fetchall():
+        articlesArray.append(article)
 
-    addArticle = AddArticleForm()
-    addArticle.category.choices = categoryArray #fill dropdown with categories
+    addArticle = ArticleForm()
+    removeArticle = RemoveArticleForm()
 
-    if addArticle.validate_on_submit() and addArticle.submitAddArticle.data:
-        print(str(addArticle.category.data))
+
+    if addArticle.validate_on_submit() and addArticle.submitArticle.data:
+        #print(str(addArticle.category.data))
+        print("Adding article")
+        #det här kan ju såklart tas bort, men sql queriet blir något lättare att läsa och pilla i.
         chooseCat = str(addArticle.category.data)
         articleName = str(addArticle.name.data)
         stockAmount = str(addArticle.stock.data)
         price = str(addArticle.price.data)
         url = str(addArticle.url.data)
-
-        
         desc = str(addArticle.description.data)
 
         cur.execute("INSERT INTO articles (article_name, category, price, stock_quantity, picture_url) VALUES ('" + articleName + "','" + chooseCat +
                      "', '" + price+ "' ,  '" + stockAmount + "'   ,'" + url + "');")
         if desc != "":
-            cur.execute("INSERT INTO article_description (art_description, description_id ) VALUES ('" + desc + "', (SELECT article_number FROM articles WHERE article_name='" + articleName + "'));")
+            cur.execute("INSERT INTO article_description (art_description, description_id ) VALUES ('" + desc +
+             "', (SELECT article_number FROM articles WHERE article_name='" + articleName + "'));")
         db.connection.commit()
         cur.close()
         return redirect(url_for('admin.adminArticles'))
 
-    return render_template("admin/articlesv2.html", addArticleForm = addArticle, articles=articlesArray)
+
+    if removeArticle.validate_on_submit() and removeArticle.submitRemoveArticle.data:
+        articleToRemove = str(removeArticle.article.data)
+        cur.execute("DELETE FROM articles WHERE article_number=" + articleToRemove + ";")
+        print("removed article")
+        db.connection.commit()
+        cur.close()
+        return redirect(url_for('admin.adminArticles'))
+
+    # elif request.form['submit'] == 'remove':
+    #     removeArticle = request.form['removefield']
+    #     cur.execute("DELETE FROM articles WHERE article_number=" + removeArticle + ";")
+    #     db.connection.commit()
+    #     cur.close()
+    #     return redirect(url_for('admin.adminArticles'))
+
+    return render_template("admin/articlesv2.html", addArticleForm = addArticle, articles=articlesArray,removeArticleForm=removeArticle)
 
 @bp.route("/admin/categories", methods=['POST', 'GET'])
 def adminCategories():
@@ -74,14 +125,15 @@ def adminCategories():
         return redirect(url_for('admin.adminCategories'))
 
 
-    elif removeCategory.validate_on_submit() and removeCategory.submitRemove.data:
+    if removeCategory.validate_on_submit() and removeCategory.submitRemove.data:
+        print("remoive")
         deleteCat = removeCategory.category_id.data
         cur.execute("DELETE FROM categories WHERE category_id=" + str(deleteCat))
         db.connection.commit()
         cur.close()
         return redirect(url_for('admin.adminCategories'))
 
-    elif editCategory.validate_on_submit() and editCategory.submitEdit.data:
+    if editCategory.validate_on_submit() and editCategory.submitEdit.data:
         catID = str(editCategory.category_id.data)
         newName = str(editCategory.new_name.data)
 
@@ -91,7 +143,8 @@ def adminCategories():
         return redirect(url_for('admin.adminCategories'))
 
 
-    return render_template("admin/categoriesv2.html", categories=categoryArray, addCategoryForm=addCategory, removeCategoryForm=removeCategory, editCategoryForm=editCategory)
+    return render_template("admin/categoriesv2.html", categories=categoryArray, addCategoryForm=addCategory, removeCategoryForm=removeCategory,
+     editCategoryForm=editCategory)
 
 
 @bp.route("/admin/users", methods=['POST', 'GET'])
@@ -111,3 +164,66 @@ def adminUsers():
             cur.close()
             return redirect(url_for('admin.adminUsers'))
     return render_template("admin/users.html", users=userArray)
+
+
+
+
+# @bp.route("/admin/articles", methods=['POST', 'GET'])
+# def adminArticles():
+#     cur = db.connection.cursor()
+#     cur.execute("SELECT category_name, category_id FROM categories")
+#     categoryArray = []
+#     for cat in cur.fetchall():
+#         categoryArray.append(cat)
+
+#     cur.execute(
+#         "SELECT article_number, article_name, stock_quantity, price FROM articles")
+#     articlesArray = []
+#     for art in cur.fetchall():
+#         articlesArray.append(art)
+
+#     if request.method == 'POST':
+#         # Submit för att lägga till artiklar
+#         if request.form['submit'] == 'addarticle':
+#             chooseCat = "('" + request.form['chooseCategory'] + "')"
+#             articleName = "('" + request.form['article'] + "')"
+#             stockAmount = request.form['stock']
+#             price = request.form['price']
+#             url = "('" + request.form['url'] + "')"
+#             desc = request.form['description']
+
+#             cur.execute("INSERT INTO articles (article_name, category, price, stock_quantity, picture_url) VALUES (" + articleName +
+#                         ",(SELECT category_id FROM categories WHERE category_name=" + chooseCat + ")," + price + "," + stockAmount + "," + url + ");")
+#             if desc != "":
+#                 desc = "('" + request.form['description'] + "')"
+#                 cur.execute("INSERT INTO article_description (description, description_number ) VALUES (" +
+#                             desc + ", (SELECT article_number FROM articles WHERE article_name=" + articleName + "));")
+#             db.connection.commit()
+#             cur.close()
+#             return redirect(url_for('admin.adminArticles'))
+#         # Ta bort artikel
+#         elif request.form['submit'] == 'remove':
+#             removeArticle = request.form['removefield']
+#             cur.execute(
+#                 "DELETE FROM articles WHERE article_number=" + removeArticle + ";")
+#             db.connection.commit()
+#             cur.close()
+#             return redirect(url_for('admin.adminArticles'))
+#         # Ändra artikelinformation
+#         elif request.form['submit'] == 'edit':
+#             editID = request.form['editfield']
+#             newName = "('" + request.form['newname'] + "')"
+#             newStock = request.form['newstock']
+#             newPrice = request.form['newprice']
+#             newDescription = request.form['newdescription']
+#             cur.execute("UPDATE articles SET article_name=" + newName + ", stock_quantity=" +
+#                         newStock + ", price=" + newPrice + " WHERE article_number= " + editID + ";")
+#             if newDescription != "":
+#                 newDescription = "('" + request.form['newdescription'] + "')"
+#                 cur.execute("UPDATE article_description SET description=" +
+#                             newDescription + " WHERE description_number= " + editID + ";")
+#             db.connection.commit()
+#             cur.close()
+#             return redirect(url_for('admin.adminArticles'))
+
+#     return render_template("admin/articles.html", categories=categoryArray, articles=articlesArray
